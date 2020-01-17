d96bf2fdf2c05fe3968e1a896caf0d7b2c8a41bb
felipep-lagash <felipep@lagash.com>
2020-01-16 23:51:16 -0300

    Se completaron endpoints

---
+++ api/src/routes/events.js
@@ -0,0 +1,44 @@
+const Router = require("restify-router").Router;
+const router = new Router();
+const sql = require("mssql");
+const sqlConfig = require("../config/database");
+
+const {
+  getAllEvents,
+  getAllTodayEvents,
+  getEvent,
+  setEvent,
+  updateEvent,
+  deleteEvent
+} = require("../services/events");
+
+router.get("/all", async (req, res) => {
+  res.send(200, await getAllEvents());
+});
+
+router.get("/allToday", async (req, res) => {
+  res.send(200, await getAllTodayEvents());
+});
+
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  res.send(200, await getEvent(id));
+});
+
+router.post("/add", async (req, res) => {
+  const { body } = req;
+  res.send(201, await setEvent(body));
+});
+
+router.put("/update/:id", async (req, res) => {
+  const { body } = req;
+  const { id } = req.params;
+  res.send(201, await updateEvent(id, body));
+});
+
+router.del("/delete/:id", async (req, res) => {
+  const { id } = req.params;
+  res.send(200, await deleteEvent(id));
+});
+
+module.exports = router;
